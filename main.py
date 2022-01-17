bfb09ea5cdfbb607a950065bccf2dd61ef23edf5
xacer <chacecaven@outlook.com>
2022-01-17 12:08:03 -0500

    Now it runs correctly on download

--- main.py
+++ main.py
@@ -15,7 +15,11 @@ from model import Transformer
 CUDA = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 CPU = torch.device("cpu")
 
-EPOCHS = 2500
+EPOCHS = 500
+
+MODEL_OUTPUT_FOLDER = "./models/"
+MIDI_OUTPUT_FOLDER = "./midi/"
+PNG_OUTPUT_FOLDER = "./png/"
 
 def load_new(ntoken):
     return Transformer(ntoken=ntoken,
@@ -80,14 +84,14 @@ def train(model: Transformer, song_batches, ntoken):
         scheduler.step()
         
         if epoch % 100 == 0:
-            torch.save(model, "models/5/" + str(2500+epoch) + "_epochs.pth")
+            torch.save(model, MODEL_OUTPUT_FOLDER + str(epoch) + "_epochs.pth")
         
         print("\n")
         print(epoch, ":", total_loss)
         print("~" * 20)
         
     # Save model
-    torch.save(model, "models/5/" + str(2500+EPOCHS) + "_epochs.pth")
+    torch.save(model, MODEL_OUTPUT_FOLDER + str(EPOCHS) + "_epochs.pth")
     
     plt.plot(total_loss_list)
     plt.xlabel("Iter")
@@ -129,17 +133,17 @@ def predict(model: Transformer, dataset: SongDataset, song_batches, ntoken):
         multitrack = piano.Multitrack("tracks", tempo=np.ones(len(notes)) * 15, tracks=[track])
         
         piano.plot(multitrack)
-        plt.savefig("generated/7-png/" + str(idx) + ".png")
+        plt.savefig(PNG_OUTPUT_FOLDER + str(idx) + ".png")
         #plt.show()
         
         midi = multitrack.to_pretty_midi()
-        midi.write("generated/7-mid/" + str(idx) + ".mid")
+        midi.write(MIDI_OUTPUT_FOLDER + str(idx) + ".mid")
         
         print("Wrote", idx, "mid and png to generated/6/")
 
 def get_total_loss_grapb(dataset: SongDataset, song_batches, ntoken):
     # find all model files
-    all_files = os.listdir("models/5")
+    all_files = os.listdir(MODEL_OUTPUT_FOLDER)
     loss_fn = torch.nn.CrossEntropyLoss()
     
     total_loss_x = []
@@ -196,34 +200,34 @@ def get_total_loss_grapb(dataset: SongDataset, song_batches, ntoken):
 
 def main():
     
-    #data = SongDataset()
-    #data.load_pickle("data/Nottingham.pickle")
-    #song_batches = data.create_batches(16, 128)
+    data = SongDataset()
+    data.load_pickle("data/Nottingham.pickle")
+    song_batches = data.create_batches(16, 128)
     
-    #ntoken = len(data.dictionary)
+    ntoken = len(data.dictionary)
     
     #print("Num tokens: ", ntoken)
     
-    #model = load_new(ntoken=ntoken)
+    model = load_new(ntoken=ntoken)
     #model = load_presaved("models/5/5000_epochs.pth")
     
     #predict(model, data, song_batches, ntoken)
-    #train(model, song_batches, ntoken)
+    train(model, song_batches, ntoken)
 
     #get_total_loss_grapb(data, song_batches, ntoken)
 
-    with open("stats/total_loss.pickle", "rb") as f:
-        x, y = pickle.load(f)
-        d = zip(x, y)
-        d = sorted(d, key=lambda a: a[0])
-        x = [i[0] for i in d]
-        y = [i[1] for i in d]
-        plt.plot(x, y)
-        #plt.xscale("log")
-        plt.title("Loss vs Epoch")
-        plt.xlabel("Epoch")
-        plt.ylabel("Loss")
-        plt.show()
+    #with open("stats/total_loss.pickle", "rb") as f:
+    #    x, y = pickle.load(f)
+    #    d = zip(x, y)
+    #    d = sorted(d, key=lambda a: a[0])
+    #    x = [i[0] for i in d]
+    #    y = [i[1] for i in d]
+    #    plt.plot(x, y)
+    #    #plt.xscale("log")
+    #    plt.title("Loss vs Epoch")
+    #    plt.xlabel("Epoch")
+    #    plt.ylabel("Loss")
+    #    plt.show()
 
     return
     
